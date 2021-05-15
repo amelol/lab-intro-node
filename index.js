350dfa2515cd0b5169a2b6aefb1252431cba0e54
amelol <75670435+amelol@users.noreply.github.com>
2021-05-15 16:13:17 +0300

    solo faltan 4 y 5

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items = this.items.sort((a, b) =>  a - b)
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.items.length || pos < 0 || this.items.length === 0) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos]
+    }
+  }
 
-  max() {}
+  // max() {
+  //   if (this.items.length === 0) {
+  //      throw new Error('EmptySortedList');
+  //   } else {
+  //     return this.items[this.items.length - 1]
+  //   }
+  // }
+  max() {
 
-  min() {}
+  }
+  min() {
 
-  sum() {}
+  }
+  // min() {
+  //   if (this.items.length === 0) {
+  //     throw new Error('EmptySortedList');
+  //   } else {
+  //     return this.items[0]
+  //   }
+  // }
 
-  avg() {}
+  sum() {
+    if (this.items.length === 0) {
+       return 0
+    } else {
+     return this.items.reduce((sum, item) => {return sum + item}, 0)
+    }
+  }
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.sum()/this.items.length
+    }
+  }
 }
 
 module.exports = SortedList;
